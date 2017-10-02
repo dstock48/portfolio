7c5a4db43eca4c45c77b9c02fc20b084a8d4ec16
Dave W <dviper@gmail.com>
2017-10-02 14:03:02 -0600

    create and style initial contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return(
+    <section id="contact" className="contact">
+      <h2 className="section-heading">{'< Contact >'}</h2>
+      <form className="email-form" method="POST" action="http://formspree.io/david.weinstock@me.com">
+        <input type="text" name="name" placeholder="Your name" />
+        <input type="email" name="email" placeholder="Your email" />
+        <textarea name="message" placeholder="Your message"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  )
+}
+
+export default Contact;
