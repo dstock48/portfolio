3e464902f34a98596e85bedce79574c59cf31810
Dave W <dviper@gmail.com>
2017-10-02 11:15:39 -0600

    Move navigation under name & tagline

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,15 @@ import './Header.css';
 const Header = () => {
   return(
     <header>
-      {/* <nav>
-        <a href="#">About</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
-      </nav> */}
       <div className="hero-text">
         <h1 className="name">David Weinstock</h1>
         <p className="tagline">Front-End Web Developer</p>
       </div>
+      <nav>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        {/* <a href="#">Contact</a> */}
+      </nav>
       <div className='icon-scroll'></div>
     </header>
   )
