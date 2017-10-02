45b4fdf84a5de5db72d0c5a833766c789ad6d62a
Dave W <dviper@gmail.com>
2017-10-02 14:16:19 -0600

    update url to https

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return(
     <section id="contact" className="contact">
       <h2 className="section-heading">{'< Contact >'}</h2>
-      <form className="email-form" method="POST" action="http://formspree.io/david.weinstock@me.com">
+      <form className="email-form" method="POST" action="https://formspree.io/david.weinstock@me.com">
         <input type="text" name="name" placeholder="Your name" />
         <input type="email" name="email" placeholder="Your email" />
         <textarea name="message" placeholder="Your message"></textarea>
