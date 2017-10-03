85da40fc816fd8b646e1a138b22518e2feed693a
Dave W <dviper@gmail.com>
2017-10-02 20:28:52 -0600

    style changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <div className="about-text">
         <img className="avatar" src={avatar} alt="Profile Pic"/>
         <p>My name is David Weinstock and I'm a front-end web developer based out of Denver, Colorado.</p>
-        <p>I just recently finished a 7 month immersive software development program at the <a href="https://www.turing.io" target="_blank">Turing School of Software & Design.</a> This program covered the core languages of JavaScript, HTML, and CSS, along with many other libraries and tools such as jQuery, React, Redux, Node, Express, Webpack, and SASS. Turing puts a great deal of attention into testing code using libraries such as Jest, Enzyme, Mocha, and Chai. Projects are completed both individually and in groups, with a large focus on pair programming and cooperative git workflows.</p>
+        <p>I just recently finished a 7 month immersive software development program at the <a rel="noopener noreferrer" href="https://www.turing.io" target="_blank">Turing School of Software & Design.</a> This program covered the core languages of JavaScript, HTML, and CSS, along with many other libraries and tools such as jQuery, React, Redux, Node, Express, Webpack, and SASS. Turing puts a great deal of attention into testing code using libraries such as Jest, Enzyme, Mocha, and Chai. Projects are completed both individually and in groups, with a large focus on pair programming and cooperative git workflows.</p>
       </div>
     </section>
   )
