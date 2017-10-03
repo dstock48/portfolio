f648c2d7bceee2d242b990797101afaab0b89d4c
Dave W <dviper@gmail.com>
2017-10-03 11:55:54 -0600

    remove heart svg file, place svg code in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Footer.css';
-import heart from '../../assets/heart.svg'
 
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
 
+  const heart = <svg className="heart-icon" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 50 50'>
+                  <path
+                    fill="#ee7b7b"
+                    d='M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543	c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503	c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z' />
+                </svg>
+
   return(
     <footer>
-      <span>Made with <img className="heart-icon" src={heart} alt="heart"/> by David Weinstock</span>
+      <span>Made with {heart} by David Weinstock</span>
       <span>Copyright &#169; {year}</span>
     </footer>
   )
