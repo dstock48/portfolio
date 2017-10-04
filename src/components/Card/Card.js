66339a928c7e612c5e7bcb674902e1305b47612a
Dave W <dviper@gmail.com>
2017-10-04 14:09:50 -0600

    redesign card UI

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,19 +9,19 @@ const Card = (props) => {
 
   return(
     <div className="project-card">
-      <div className="card-text">
-        <h3 className="project-name">{props.name}</h3>
-        <p className="description">{props.desc}</p>
-        <div className="links">
-          {props.repoLink && <a href={props.repoLink} target="_blank">View on GitHub</a>}
-          {props.liveLink && <span> • <a href={props.liveLink} target="_blank">View Live</a></span>}
-        </div>
-      </div>
-      {/* <div style={style} className="project-screenshot"></div> */}
       <div className="card-image">
         <div className="color-overlay"></div>
         <img className="screenshot" src={props.image} alt="screenshot"/>
       </div>
+      <div className="card-text">
+        <h3 className="project-name">{props.name}</h3>
+        <div className="links">
+          {props.repoLink && <a href={props.repoLink} target="_blank">View on GitHub</a>}
+          {props.liveLink && <span> • <a href={props.liveLink} target="_blank">View Live</a></span>}
+        </div>
+        <p className="description">{props.desc}</p>
+      </div>
+      {/* <div style={style} className="project-screenshot"></div> */}
     </div>
   )
 }
