76fa0d869e92cc1f2b71417ee00a1ff7625e6963
Dave W <dviper@gmail.com>
2017-10-04 11:06:50 -0600

    update all the stuff!

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,18 @@ import './Card.css';
 
 const Card = (props) => {
 
-  const style = {
-    backgroundImage: `url(${props.image})`
-  }
+  // const style = {
+  //   backgroundImage: `url(${props.image})`
+  // }
 
   return(
     <a href={props.link} target="_blank" className="project-card">
-      <h3 className="project-name">{props.name}</h3>
-      <div style={style} className="project-screenshot"></div>
+      <div className="card-text">
+        <h3 className="project-name">{props.name}</h3>
+        <p className="description">{props.desc}</p>
+      </div>
+      {/* <div style={style} className="project-screenshot"></div> */}
+      <img className="screenshot" src={props.image} alt="screenshot"/>
     </a>
   )
 }
--- src/helpers/projectData.js
+++ src/helpers/projectData.js
@@ -1,33 +1,47 @@
+import whosPlaying from '../assets/screenshots/whosplaying.png';
+import clubReads from '../assets/screenshots/clubreads.png';
+import movieTracker from '../assets/screenshots/movietracker.jpg';
+import weathrly from '../assets/screenshots/weathrly.png';
+import comp2 from '../assets/screenshots/comp2.jpg';
+import comp3 from '../assets/screenshots/comp3.jpg';
+
+
 const projectData = [
   {
     name: 'WhosPlaying',
-    screenshot: 'https://raw.githubusercontent.com/dstock48/whosplaying/master/screenshots/whosplaying1.png',
+    screenshot: whosPlaying,
     link: 'https://github.com/dstock48/whosplaying',
+    description: 'A web app to find new artists and local concerts in your area, or search other locations',
   },
   {
     name: 'Club Reads',
-    screenshot: 'https://user-images.githubusercontent.com/20492875/30620618-b337c5ce-9d63-11e7-925b-d3f878094068.png',
+    screenshot: clubReads,
     link: 'https://github.com/lindsaywparker/club-reads-frontend',
+    description: 'A web app for book club members to suggest & vote on books to read',
   },
   {
     name: 'Movie Tracker',
-    screenshot: 'https://user-images.githubusercontent.com/20492875/30605948-4277fe0c-9d2d-11e7-9dda-8911615b092c.jpg',
+    screenshot: movieTracker,
     link: 'https://github.com/dstock48/movie-tracker',
+    description: 'A React/Redux project building a "New Movie Releases" grid interface connected to an express backend storing user accounts and favorites',
   },
   {
     name: 'Weathrly',
-    screenshot: 'https://user-images.githubusercontent.com/20492875/30603287-45f53576-9d24-11e7-8e26-9bda0a81115a.png',
+    screenshot: weathrly,
     link: 'https://github.com/dstock48/weathrly',
+    description: 'A weather app built in react.js using the Weather Underground API',
   },
   {
     name: 'Dashboard Design Comp',
-    screenshot: 'https://raw.githubusercontent.com/dstock48/dw-comp-challenge-3/master/screenshots/Final.jpg',
+    screenshot: comp3,
     link: 'https://dstock48.github.io/dw-comp-challenge-3/',
+    description: 'A static design challenge using flexbox, absolute & relative positioning, and media queries for full responsiveness',
   },
   {
     name: 'Card Layout Comp',
-    screenshot: 'https://raw.githubusercontent.com/dstock48/dw-comp-challenge-2/master/screenshots/final.jpg',
+    screenshot: comp2,
     link: 'https://dstock48.github.io/dw-comp-challenge-2/',
+    description: 'A static design challenge using flexbox to create a grid interface of cards with hover effects and full responsiveness',
   },
 ]
 
