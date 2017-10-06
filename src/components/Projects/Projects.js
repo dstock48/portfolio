55d9ba9bc0cd1d966407f824303b511cec9f5d01
Dave W <dviper@gmail.com>
2017-10-06 10:36:42 -0600

    Add altText props to card component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,19 @@ import './Projects.css';
 const Element = Scroll.Element;
 
 
-const projectCards = projectData.map((proj, i) => <Card key={`Project-${i}`} name={proj.name} image={proj.screenshot} repoLink={proj.repoLink} liveLink={proj.liveLink} desc={proj.description}/>)
+const projectCards = projectData.map((proj, i) => {
+  return (
+    <Card
+      key={`Project-${i}`}
+      name={proj.name}
+      image={proj.screenshot}
+      repoLink={proj.repoLink}
+      liveLink={proj.liveLink}
+      desc={proj.description}
+      altText={proj.altText}
+    />
+  )
+})
 
 const Projects = () => {
 
