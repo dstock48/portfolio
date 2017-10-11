32f8665bfd8eca4e1d7a6c52477ba76c84fe0aa4
Dave W <dviper@gmail.com>
2017-10-11 10:44:09 -0600

    Update About text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,8 +12,8 @@ const About = () => {
         <div className="about-content">
           <img className="avatar" src={avatar} alt="Headshot"/>
           <div className="text">
-            <p>My name is David Weinstock and I'm a front-end web developer based out of Denver, Colorado.</p>
-            <p>I just recently completed a 7 month immersive software development program at the <a rel="noopener noreferrer" href="https://www.turing.io" target="_blank">Turing School of Software & Design.</a> This program covered the core languages of JavaScript, HTML, and CSS, along with many other libraries and tools such as jQuery, React, Redux, Node, Express, Webpack, and SASS. Turing puts a great deal of attention into testing code using libraries such as Jest, Enzyme, Mocha, and Chai. Projects are completed both individually and in groups, with a large focus on pair programming and cooperative git workflows.</p>
+            <p>My name is David Weinstock, and I'm a frontend web developer based out of Denver, Colorado. I have a passion for clean design and playful, intuitive user interfaces.</p>
+            <p>I recently completed a 7-month immersive software development program at the <a rel="noopener noreferrer" href="https://www.turing.io" target="_blank">Turing School of Software & Design.</a> This program covered the core languages of JavaScript, HTML, and CSS, along with many other libraries and tools such as jQuery, React, Redux, Node, Express, Webpack, and SASS. Turing puts a great deal of attention into testing code using libraries such as Jest, Enzyme, Mocha, and Chai. Projects are completed both individually and in groups, with a significant focus on pair programming and cooperative git workflows.</p>
           </div>
         </div>
       </section>
